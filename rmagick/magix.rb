0aae4598a232f90b1aa478ce739325e2d38245a6
sourcx <wh@y42.nl>
2015-12-08 11:53:13 +0100

    rmagick en thread

--- rmagick/magix.rb
+++ rmagick/magix.rb
@@ -2,10 +2,14 @@ require 'rmagick'
 
 image = Magick::Image.read("images/full_man.png").first
 
-tie = Magick::Image.read("images/tie_1.png").first
+tie = Magick::Image.read("images/tie_2.png").first
 image.composite!(tie, 0, 0, Magick::OverCompositeOp)
 
-suit = Magick::Image.read("images/suit_0.png").first
+
+shirt = Magick::Image.read("images/shirt_0.png").first
+image.composite!(shirt, 0, 0, Magick::OverCompositeOp)
+
+suit = Magick::Image.read("images/suit_3.png").first
 image.composite!(suit, 0, 0, Magick::OverCompositeOp)
 
 image.write("images/out.png")
