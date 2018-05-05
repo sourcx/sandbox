e124f51d170ece99e757eee79f118236eba51be4
sourcx <wh@y42.nl>
2018-05-05 12:34:58 -0400

    greetings

--- encodefont/encode.rb
+++
@@ -1,7 +0,0 @@
-require 'base64'
-
-# file = File.binread("./din-medium.ttf")
-# enc = Base64.encode64(file)
-# puts enc
-
-Base64.encode64(file.open('/assets/abadi_mt_condensed_light_regular-webfont.ttf') { |io| io.read })
